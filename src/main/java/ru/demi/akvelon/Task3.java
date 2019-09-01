71469bf2763870c4a02b4483e838d845d7f024ca
dmitry <dmitrii_izmerov@epam.com>
2019-09-01 12:55:53 +0300

    solved third akvelon task

--- src/main/java/ru/demi/akvelon/Task3.java
+++ src/main/java/ru/demi/akvelon/Task3.java
@@ -4,10 +4,13 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * Unsolved
+ * Anagram difference
  */
 public class Task3 {
     public static void main(String[] args) throws IOException {
@@ -21,7 +24,6 @@ public class Task3 {
             bw = new BufferedWriter(new OutputStreamWriter(System.out));
         }
 
-        int[] res;
         int a_size = 0;
         a_size = Integer.parseInt(in.nextLine().trim());
 
@@ -50,41 +52,41 @@ public class Task3 {
             b[i] = b_item;
         }
 
-        res = getMinimumDifference(a, b);
-        for(int res_i = 0; res_i < res.length; res_i++) {
-            bw.write(String.valueOf(res[res_i]));
+        List<Integer> res = getMinimumDifference(Arrays.asList(a), Arrays.asList(b));
+        for(int res_i = 0; res_i < res.size(); res_i++) {
+            bw.write(String.valueOf(res.get(res_i)));
             bw.newLine();
         }
 
         bw.close();
     }
 
-    static int[] getMinimumDifference(String[] a, String[] b) {
-        int[] r = new int[a.length];
+    static List<Integer> getMinimumDifference(List<String> a, List<String> b) {
+        List<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].length() != b[i].length()) {
-                r[i] = -1;
+        for (int i = 0; i < a.size(); i++) {
+            String first = a.get(i);
+            String second = b.get(i);
+
+            if (first.length() != second.length()) {
+                result.add(-1);
                 continue;
             }
 
-            String s1 = new StringBuilder(a[i]).reverse().toString();
-            String s2 = new StringBuilder(b[i]).reverse().toString();
-
-            if (s1.equals(b[i]) || s2.equals(a[i])) {
-                r[i] = 0;
-                continue;
+            int[] charsCount = new int[26];
+            for (int j = 0; j < first.length(); j++) {
+                ++charsCount[first.charAt(j) - 'a'];
             }
 
-            int c = 0;
-            for (int j = 0; j < a[i].length(); j++) {
-                if (a[i].charAt(j) != s2.charAt(j) || s1.charAt(j) != b[i].charAt(j)) {
-                     ++c;
+            int count = 0;
+            for (int j = 0; j < second.length(); j++) {
+                if (--charsCount[second.charAt(j) - 'a'] < 0) {
+                    ++count;
                 }
             }
-            r[i] = c;
+            result.add(count);
         }
 
-        return r;
+        return result;
     }
 }
